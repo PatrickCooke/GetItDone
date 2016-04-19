5d45fbf887210da60b8b74ea1c14fc9a8992b4eb
PatrickCooke <cookepa1@gmail.com>
2016-04-19 12:18:11 -0400

    Initial Commit

---
+++ GetItDone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetItDone
+//
+//  Created by Patrick Cooke on 4/19/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
