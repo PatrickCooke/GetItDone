b348b833d7c689cc8781b6e6e3fbd5833e51cbf8
PatrickCooke <cookepa1@gmail.com>
2016-04-19 16:35:25 -0400

    detail screen date pickers and segmented controller wired up and working

--- GetItDone/DetailViewController.m
+++ GetItDone/DetailViewController.m
@@ -10,8 +10,12 @@
 
 @interface DetailViewController ()
 
+
+@property(nonatomic,weak) IBOutlet UINavigationBar    *todoNametitle;
 @property(nonatomic,weak) IBOutlet UILabel    *todoNameLabel;
-@property(nonatomic,weak) IBOutlet UILabel    *todopriorityLabel;
+@property(nonatomic,weak) IBOutlet UISegmentedControl    *todoprioritySegControl;
+@property(nonatomic,weak) IBOutlet UIDatePicker         *todoOptionalDatePicker;
+@property(nonatomic,weak) IBOutlet UIDatePicker         *todoFinalDatePicker;
 @property(nonatomic,weak) IBOutlet UILabel    *todoOptionalDateLabel;
 @property(nonatomic,weak) IBOutlet UILabel    *todoDeadlineDateLable;
 @property(nonatomic,weak) IBOutlet UITextView *todoDescriptionTextView;
@@ -22,12 +26,30 @@
 
 @implementation DetailViewController
 
+#pragma mark - Detail Change Method
+
+-(IBAction)savebutton:(id)sender {
+
+}
+
+
+
+
+#pragma mark - Life Cycle Methods
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSDateFormatter *dateFormater = [[NSDateFormatter alloc] init];
+    [dateFormater setDateFormat:@"M-dd-yyyy"];
+    NSDate *optionalDueDate = _selectedToDo.todoOptionalDueDate;
+    NSDate *finalDueDate = _selectedToDo.todoCompletionDate;
     _todoNameLabel.text = _selectedToDo.todoName;
-    _todopriorityLabel.text = [NSString stringWithFormat:@"This is a %@ priority ToDo",_selectedToDo.todoPriority];
-    _todoOptionalDateLabel.text = [NSString stringWithFormat:@"%@", _selectedToDo.todoOptionalDueDate];
-    _todoDeadlineDateLable.text = [NSString stringWithFormat:@"%@", _selectedToDo.todoCompletionDate];
+    NSInteger todoPriorityInt = _selectedToDo.todoPriority.integerValue;
+    _todoprioritySegControl.selectedSegmentIndex = todoPriorityInt - 1;
+
+    _todoOptionalDatePicker.date = optionalDueDate;
+    _todoFinalDatePicker.date = finalDueDate;
+    //_todoOptionalDateLabel.text = [NSString stringWithFormat:@"Optional Due Date %@", optionalDueDateString];
+    //_todoDeadlineDateLable.text = [NSString stringWithFormat:@"Final Due Date %@", finalDueDateString];
     _todoDescriptionTextView.text = _selectedToDo.todoDescription;
 }
 
